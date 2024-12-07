0eaf45f48ba7a4b5445b34657666fa721481814f
CerenTarim <ftarim19@ku.edu.tr>
2024-12-07 16:18:58 +0300

    piping bitti :))

--- src/shell-skeleton.c
+++ src/shell-skeleton.c
@@ -444,6 +444,42 @@ int process_command(struct command_t *command) {
 		else {
 
 		}
+
+		if (command->next) {
+			int pipefd[2];
+			if (pipe(pipefd) == -1) {
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
+
+			pid_t pid1 = fork();
+			if (pid1 == 0) {
+				close(pipefd[0]); 
+				dup2(pipefd[1], STDOUT_FILENO); 
+				close(pipefd[1]); 
+				execvp(command->name, command->args);
+				perror("execvp"); 
+				exit(EXIT_FAILURE);
+			}
+
+			pid_t pid2 = fork();
+			if (pid2 == 0) {
+				close(pipefd[1]); 
+				dup2(pipefd[0], STDIN_FILENO); 
+				close(pipefd[0]); 
+				execvp(command->next->name, command->next->args);
+				perror("execvp"); 
+				exit(EXIT_FAILURE);
+			}
+
+			// Parent process
+			close(pipefd[0]); 
+			close(pipefd[1]);
+			waitpid(pid1, NULL, 0); 
+			waitpid(pid2, NULL, 0); 
+			return SUCCESS;
+		}
+
 		wait(0); // wait for child process to finish
 		return SUCCESS;
 	}
